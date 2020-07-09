05fecc49d28f02ab0cb5a48d7646bf508a3733a0
hannah levardon <hlevardon@gmail>
2020-07-09 15:39:10 +0200

    my sql changements

--- 01_partie_MySQL/LMD_manip_app_voyage.sql
+++ 01_partie_MySQL/LMD_manip_app_voyage.sql
@@ -32,6 +32,20 @@ insert into commandes(date_commande, client_id) values('2020-07-05', 1);
 insert into produits(nom_produit, description_produit, prix_produit) values('Paris', 'voyage 2 jours dans la capitale francaise', 350.00);
 insert into produits(nom_produit, description_produit, prix_produit) values('Chine', 'voyage 15 jours pour voir la grande muraille', 2350.00);
 
+insert into produits(nom_produit, description_produit, prix_produit, quantite_produit, selectionne_produit, image_produit ) values (); 
+
+update produits set nom_produit='' , description_produit='', prix_produit=0.0 , 
+quantite_produit= 0, selectionne_produit=0, image_produit=0 
+where id_produit=1 ; 
+
+delete from produits where id_produit = 1; 
+
+select * from produits where nom_produit like '%paris%' or description_produit like '%paris%';
+
+select * from produits where selectionne_produit =0; 
+
+
+
 
 insert into categories(id_categorie, nom_categorie, description_categorie)values (1, 'france', 'voyage en france metropolitaine');
 insert into categories(id_categorie, nom_categorie, description_categorie)values (2, 'etranger', 'voyage hors de france metropolitaine');
@@ -39,7 +53,7 @@ insert into categories(id_categorie, nom_categorie, description_categorie)values
 
 
 insert into produits_categories(categorie_id, produit_id) values(1, 1);
-insert into produits_categories(categorie_id, produit_id) values(2, 2);
+insert into produits_categories(categorie_id, produit_id) values(2, 3);
 insert into produits_categories(categorie_id, produit_id) values(3, 1);
 
 
@@ -61,12 +75,21 @@ insert into produits_categories(categorie_id, produit_id) values(3, 1);
 	from produits p 
 	left join produits_categories pc
 	on p.id_produit = pc.produit_id;
-   
-	select nom_produit, description_produit, prix_produit, nom_categorie, description_categorie
+    
+    select * from produit_categ;
+    
+    select * 
+    from produit_categ 
+    where categorie_id = 2;
+    
+    
+    	select nom_produit, description_produit, prix_produit, nom_categorie, description_categorie
 	from produit_categ p_cat
 	left join categories c
-	on c.id_categorie = p_cat.categorie_id
+	on c.id_categorie = p_cat.categorie_id = 1
     order by nom_produit;
+    
 	
+    
     
     
