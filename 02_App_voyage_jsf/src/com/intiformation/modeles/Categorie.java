bd8d17e0759589b1d318af7b52019b82c0561391
anais leseur <anais.leseur@gmail.com>
2020-07-09 14:48:52 +0200

    modele categorie

---
+++ 02_App_voyage_jsf/src/com/intiformation/modeles/Categorie.java
@@ -0,0 +1,80 @@
+package com.intiformation.modeles;
+
+
+/**
+ * Implémentation concrète de la DAO pour une catégorie
+ * mappé sur la table 'categories' de la bdd 
+ * 
+ * @author vincent
+ *
+ */
+public class Categorie {
+	
+	// ---Props ---
+	private int id_categorie;
+	private String nom_categorie;
+	private String description_categorie;
+	private String urlImageProduit;
+	
+	
+	
+	// ---Ctors ---
+	
+	// ctor vide
+	public Categorie() {
+	}// end ctor vide
+	
+	// ctor chargé complet
+	public Categorie(int id_categorie, String nom_categorie, String description_categorie, String urlImageProduit) {
+		this.id_categorie = id_categorie;
+		this.nom_categorie = nom_categorie;
+		this.description_categorie = description_categorie;
+		this.urlImageProduit = urlImageProduit;
+	}// end ctor chargé complet
+	
+	// ctor chargé sans id
+	public Categorie(String nom_categorie, String description_categorie, String urlImageProduit) {
+		this.nom_categorie = nom_categorie;
+		this.description_categorie = description_categorie;
+		this.urlImageProduit = urlImageProduit;
+	}// end ctor chargé sans id
+
+	
+	// ---meths ---
+	// ---Getters/setters ---
+	
+	
+	public int getId_categorie() {
+		return id_categorie;
+	}
+
+	public void setId_categorie(int id_categorie) {
+		this.id_categorie = id_categorie;
+	}
+
+	public String getNom_categorie() {
+		return nom_categorie;
+	}
+
+	public void setNom_categorie(String nom_categorie) {
+		this.nom_categorie = nom_categorie;
+	}
+
+	public String getDescription_categorie() {
+		return description_categorie;
+	}
+
+	public void setDescription_categorie(String description_categorie) {
+		this.description_categorie = description_categorie;
+	}
+
+	public String getUrlImageProduit() {
+		return urlImageProduit;
+	}
+
+	public void setUrlImageProduit(String urlImageProduit) {
+		this.urlImageProduit = urlImageProduit;
+	}
+	
+
+}// end Categorie
