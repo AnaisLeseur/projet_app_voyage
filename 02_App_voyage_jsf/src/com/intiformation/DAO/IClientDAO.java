e77f061c8494afa033e99cf6e0025dc5bde854a1
hannah levardon <hlevardon@gmail>
2020-07-17 16:43:45 +0200

    ajout de la partie Mon compte xhtml

--- 02_App_voyage_jsf/src/com/intiformation/DAO/IClientDAO.java
+++ 02_App_voyage_jsf/src/com/intiformation/DAO/IClientDAO.java
@@ -7,5 +7,7 @@ public interface IClientDAO extends IGenerique<Client> {
 	// meth spé à client
 	
 	public boolean isClientExists(String pIdentifiant, String pMdp);
+	
+	public Client getByIdAndMdp(String pIdentifiant, String pMdp);
 
 }// end interface
